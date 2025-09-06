b258a445d93a03df0cb863458e29462fd667d3af
Nehareddy232003 <pothannagari.nehareddy@gmail.com>
2025-09-06 16:57:10 +0530

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -5,4 +5,8 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   plugins: [react()],
   base: '/book-explorer/',
+    preview: {
+    allowedHosts: true, // Allow CodeSandbox preview hosts
+    port: 5173          // Optional, default is 4173
+  }
 })
