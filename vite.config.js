35bcb2f7c5312e1da2a1d6cc1e0bf9188a034836
Neha reddy <pothannagari.nehareddy@gmail.com>
2025-09-06 16:08:38 +0000

    fix: updated vite config and dependencies

--- vite.config.js
+++ vite.config.js
@@ -1,11 +1,14 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/book-explorer/',
+  base: "/book-explorer/",
   preview: {
     allowedHosts: true, // ✅ Allow CodeSandbox host
   },
-})
+  server: {
+    allowedHosts: ["k8j6jl-5173.csb.app"], // 👈 add this line
+  },
+});
